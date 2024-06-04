46a03902382b2e1d3ef3b2ac3632d98a1b074dae
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-06-04 18:07:58 +0300

    Completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    num = 10
+    while ( num > 0 ) :
+        print ( num )
+        num -= 1
+    print ( "Happy New Year!" )
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared = [ numbers*numbers for numbers in int_list ]
+    return squared
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range ( 1, 101 ) :
+        if i % 3 == 0 and i % 5 == 0 :
+            print ( "FizzBuzz" )
+        elif i % 3 == 0 :
+            print ( "Fizz" )
+        elif i % 5 == 0 :
+            print ( "Buzz" )
+        else :
+            print ( i )
+
+fizzbuzz()
